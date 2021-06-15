74e8a04a2b39d656e1fa60d39494a8e63a423eee
Bruno Mituti <bmituti24@gmail.com>
2021-06-15 22:31:32 +0900

    Implementing bootstrap

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Packages Host') }}</title>
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <style>
+        .bg-danger-light{
+            background-color: #f2dede !important
+        }
+        .bg-success-light{
+            background-color: #dff0d8 !important
+        }
+    </style>
+    @livewireStyles
+</head>
+<body class="bg-dark">
+    <div id="app">
+        <main class="py-4">
+            @if (isset($errors) && $errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>
